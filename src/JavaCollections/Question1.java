f5ec3c742988d70a3dc469db1aa2c9a4aee44fcf
Preeti Chib <preeti.chib@tothenew.com>
2025-02-07 00:33:03 +0530

    added q1 description

--- src/JavaCollections/Question1.java
+++ src/JavaCollections/Question1.java
@@ -1,5 +1,7 @@
 package JavaCollections;
-
+/*Q1. Write Java code to define List.
+Insert 5 floating point numbers in List, and using an iterator,
+find the sum of the numbers in List.*/
 import java.util.*;
 
 public class Question1 {
