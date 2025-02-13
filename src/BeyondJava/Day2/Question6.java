002550f35c36e9825087a5172d9c171051f3c3d2
Preeti Chib <preeti.chib@tothenew.com>
2025-02-14 05:09:35 +0530

    Beyond java sequenced set

--- src/BeyondJava/Day2/Question6.java
+++ src/BeyondJava/Day2/Question6.java
@@ -5,11 +5,14 @@ package BeyondJava.Day2;
 * getFirst(), getLast(), reversed() in
 * Set and List Sequenced collections*/
 
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.SequencedCollection;
+import java.util.SequencedSet;
 
 public class Question6 {
     public static void main(String[] args) {
+        System.out.println("-----Sequenced List Operations-----");
         SequencedCollection<String> list = new LinkedList<>();
         list.addFirst("Kiwi");
         list.addLast("Orange");
@@ -23,5 +26,24 @@ public class Question6 {
         list.removeLast();
         System.out.println("After removeLast(): "+list);
         System.out.println("Reversed List: "+list.reversed());
+
+
+        System.out.println("-----Sequenced Set Operations-----");
+        SequencedSet<String> set = new LinkedHashSet<>();
+
+        set.addFirst("Strawberry");
+        set.addLast("Butterscotch");
+        set.addLast("Cotton Candy");
+        set.addLast("Cotton Candy");
+
+        System.out.println("Set: "+set);
+        System.out.println("First Element: "+set.getFirst());
+        System.out.println("Last Element: "+set.getLast());
+        set.removeFirst();
+        System.out.println("After removeFirst(): "+set);
+        set.removeLast();
+        System.out.println("After removeLast(): "+set);
+        set.addFirst("Chocolate");
+        System.out.println("Reversed Set: "+set.reversed());
     }
 }
