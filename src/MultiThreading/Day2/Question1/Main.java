10ba940daae215842c5f8a5c51f0073cdb172296
Preeti Chib <preeti.chib@tothenew.com>
2025-02-21 12:45:55 +0530

    Question 1

---
+++ src/MultiThreading/Day2/Question1/Main.java
@@ -0,0 +1,37 @@
+package MultiThreading.Day2.Question1;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // Callable Task
+        Callable<String> callable = () -> {
+            System.out.println("Simulating some string transformation from callable...");
+            Thread.sleep(3000);
+            return "This is a string from callable after transformation";
+        };
+        FutureTask<String> futureTask = new FutureTask<>(callable);
+        Thread callableThread = new Thread(futureTask);
+
+        // Runnable Task
+        String[] stringObject = new String[1];
+        Thread runnableThread = new Thread(()->{
+
+            System.out.println("Simulating some string transformation from runnable...");
+            try { // try catch mandatory for runnable
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            stringObject[0]= "This is not directly returned by runnable";
+        });
+
+
+        runnableThread.start();
+        callableThread.start();
+        System.out.println("Result: " + futureTask.get());
+        runnableThread.join();
+        System.out.println(stringObject[0]);
+    }
+}
